bf81416d8219f10b7ebf6e054a062ae443670ea5
Philipp Maier <25420070+philmph@users.noreply.github.com>
2026-09-13 13:35:09 +0200

    style: Reformatted repositories list and removed stale comment

--- renovate-bot-config/config.js
+++ renovate-bot-config/config.js
@@ -6,9 +6,14 @@ module.exports = {
     extends: ["github>philmph/renovate-bot//renovate-repo-config/default.json"],
   },
   platform: "github",
-  repositories: ["philmph/Cloudflare", "philmph/Cloudflare-Data", "philmph/GitHub-Repositories", "philmph/philmph.github.io", "philmph/Renovate-Bot"],
+  repositories: [
+    "philmph/Cloudflare",
+    "philmph/Cloudflare-Data",
+    "philmph/GitHub-Repositories",
+    "philmph/philmph.github.io",
+    "philmph/Renovate-Bot",
+  ],
 
-  // From Repository Config - No idea why this works here
   branchPrefix: "renovate/",
   gitAuthor: "Renovate Bot <bot@renovateapp.com>",
 };
